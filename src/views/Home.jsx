8aa85610a728733c283b2d6dc7daacce695b632d
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-04-28 23:08:59 -0400

    act_torta

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,13 @@
 
+import torta from "src/assets/img/torta.png"
+
+
 const Home = ()=>{
     return(
         <div className="ContHome">
             <div className="bienv">Bienvenido a <strong>Happy Cake</strong></div>
             <div className="llamado">El lugar de los pasteles felices</div>
-            <img className="imagen" src="./src/assets/img/torta.png" alt="Torta" />
+            <img className="imagen" src={torta} alt="Torta" />
         </div>
     );
 };
