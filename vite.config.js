2b4e25d96fe074d9b6aae639143aededf55161f0
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-04-28 22:52:43 -0400

    update vite.conf

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://rpavezg.github.io/desafio_happy_cake/'
 })
